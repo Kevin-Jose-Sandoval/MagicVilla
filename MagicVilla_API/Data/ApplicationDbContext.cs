fe21b94b3dc50f16038986fb3dee220ccbbf83a5
Kevin Sandoval <kevin.sandoval0026@gmail.com>
2023-03-03 11:52:07 -0600

    Create table from model and insert initial data

---
+++ MagicVilla_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Villa> Villas { get; set; }
+
+        // initial data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id= 1,
+                    Name= "Villa Real",
+                    Detail= "Detalle de la Villa...",
+                    ImageUrl= "",
+                    Capacity= 5,
+                    SquareMeter= 50,
+                    Cost= 200,
+                    Amenity= "",
+                    CreatedDate= DateTime.Now,
+                    UpdatedDate= DateTime.Now,
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Name = "Premium Vista a la Piscina",
+                    Detail = "Detalle de la Villa...",
+                    ImageUrl = "",
+                    Capacity = 4,
+                    SquareMeter = 40,
+                    Cost = 150,
+                    Amenity = "",
+                    CreatedDate = DateTime.Now,
+                    UpdatedDate = DateTime.Now,
+                }
+            );
+        }
+    }
+}
---
+++ MagicVilla_API/Migrations/20230303174749_AddInitialRecords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInitialRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenity", "Capacity", "Cost", "CreatedDate", "Detail", "ImageUrl", "Name", "SquareMeter", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, "", 5, 200.0, new DateTime(2023, 3, 3, 11, 47, 49, 298, DateTimeKind.Local).AddTicks(369), "Detalle de la Villa...", "", "Villa Real", 50, new DateTime(2023, 3, 3, 11, 47, 49, 298, DateTimeKind.Local).AddTicks(439) },
+                    { 2, "", 4, 150.0, new DateTime(2023, 3, 3, 11, 47, 49, 298, DateTimeKind.Local).AddTicks(446), "Detalle de la Villa...", "", "Premium Vista a la Piscina", 40, new DateTime(2023, 3, 3, 11, 47, 49, 298, DateTimeKind.Local).AddTicks(448) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
