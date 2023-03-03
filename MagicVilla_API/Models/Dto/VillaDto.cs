4decc6332cd836879ef02a5ad0a595d560d0d6ba
Kevin Sandoval <kevin.sandoval0026@gmail.com>
2023-03-03 10:45:09 -0600

    Update model and DTO

--- MagicVilla_API/Models/Dto/VillaDto.cs
+++ MagicVilla_API/Models/Dto/VillaDto.cs
@@ -8,7 +8,12 @@ namespace MagicVilla_API.Models.Dto
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
+        public string Detail { get; set; }
+        [Required]
+        public double Cost { get; set; }
         public int Capacity { get; set; }
         public int SquareMeter { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
--- MagicVilla_API/Models/Villa.cs
+++ MagicVilla_API/Models/Villa.cs
@@ -1,9 +1,21 @@
-﻿namespace MagicVilla_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Models
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Detail { get; set; }
+        [Required]
+        public double Cost{ get; set; }
+        public int Capacity { get; set; }
+        public int SquareMeter { get; set; }
+        public string ImageUrl{ get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate{ get; set; }
+        public DateTime UpdatedDate { get; set; }
+
     }
 }
